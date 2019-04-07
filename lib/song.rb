237dda487fe9929ba6b286f22adba55f59796b97
Kat B <katreesebarnes@gmail.com>
2019-04-07 20:34:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,9 +37,14 @@ class Song
      self.all.sort_by!{|x| x.name}
  end
  def self.new_from_filename(filename)
-   
-   
-   
+   parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
  end
 
 end
